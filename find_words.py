808da9572210de4b823c5f85291fd85555d6eb30
TC Fraser <tcfraser237@gmail.com>
2019-08-27 19:19:24 -0400

    generated truncation chains

---
+++ find_words.py
@@ -0,0 +1,28 @@
+if __name__ == '__main__':
+    with open('words_list.txt') as all_words_raw:
+        all_words = all_words_raw.read().split()
+
+    all_words.sort(key = len) # sort word list by length
+    print("Number of words = ", len(all_words))
+
+    example_word = 'antsy' 
+
+    truncation_chains = []
+
+    for cur_word in all_words:
+        if len(cur_word) == 1:
+            truncation_chains.append([cur_word])
+        else:
+            truncation = cur_word[:-1]
+            for previous_chain in truncation_chains:
+                if previous_chain[-1] == truncation:
+                    truncation_chains.append(previous_chain + [cur_word])
+                    break
+    truncation_chains.sort(key = len, reverse=True)
+    print("Longest found = ", truncation_chains[0])
+
+    output_string = '\n'.join([' > '.join(chain) for chain in truncation_chains])
+
+    with open('sorted_truncation_chains.txt', "w") as output_file:
+        output_file.write(output_string)
+        output_file.close()
