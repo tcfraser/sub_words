03181d915e693d69736cffd793418bdae97417a4
TC Fraser <tcfraser237@gmail.com>
2019-08-27 19:28:06 -0400

    used a better word list

--- find_words.py
+++ find_words.py
@@ -1,11 +1,9 @@
 if __name__ == '__main__':
-    with open('words_list.txt') as all_words_raw:
+    with open('word_list_2.txt') as all_words_raw:
         all_words = all_words_raw.read().split()
 
     all_words.sort(key = len) # sort word list by length
     print("Number of words = ", len(all_words))
-
-    example_word = 'antsy' 
 
     truncation_chains = []
 
@@ -18,11 +16,13 @@ if __name__ == '__main__':
                 if previous_chain[-1] == truncation:
                     truncation_chains.append(previous_chain + [cur_word])
                     break
+
     truncation_chains.sort(key = len, reverse=True)
-    print("Longest found = ", truncation_chains[0])
+    if len(truncation_chains) > 0:
+        print("Longest found = ", truncation_chains[0])
 
     output_string = '\n'.join([' > '.join(chain) for chain in truncation_chains])
 
-    with open('sorted_truncation_chains.txt', "w") as output_file:
+    with open('sorted_truncation_chains_2.txt', "w") as output_file:
         output_file.write(output_string)
         output_file.close()
